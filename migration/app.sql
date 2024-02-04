3ea74c4225740537bad894985936c71547e7c8f1
Minh Quan <minhquan862004@gmail.com>
2024-02-04 15:39:59 +0200

    Update table in Postgresql

--- migration/app.sql
+++ migration/app.sql
@@ -1,25 +1,32 @@
 CREATE TABLE users (
-	user_id INT NOT NULL PRIMARY KEY,
+	user_id SERIAL PRIMARY KEY,
 	user_avatar BYTEA,
 	username VARCHAR(225) NOT NULL,
 	description VARCHAR(225) NOT NULL,
 	email VARCHAR(225) NOT NULL,
-	passwords VARCHAR(225) NOT NULL
+	passwords VARCHAR(225) NOT NULL,
+	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_date TIMESTAMP
 );
 
 CREATE TABLE posts (
-    post_id INT NOT NULL PRIMARY KEY,
-	user_id INT NOT NULL,
-	created_date DATE NOT NULL,
+    post_id SERIAL PRIMARY KEY,
+	post_content,
 	like_count INT NOT NULL,
 	comment_count INT NOT NULL,
 	repost_count INT NOT NULL,
 	repost_id INT,
+	created_date DATE NOT NULL,
+	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_date TIMESTAMP,
+	user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE post_like (
-	like_id INT PRIMARY KEY,
+	like_id SERIAL PRIMARY KEY,
+	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_date TIMESTAMP,
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
@@ -27,8 +34,10 @@ CREATE TABLE post_like (
 );
 
 CREATE TABLE post_comment (
-	comment_id INT PRIMARY KEY,
+	comment_id SERIAL PRIMARY KEY,
 	comment_content VARCHAR(225),
+	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_date TIMESTAMP,
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
