e78237b098cc9b2c20c4fbab898a06789c763afe
Minh Quan <minhquan862004@gmail.com>
2024-02-05 22:24:26 +0200

    Update database

--- migration/app.sql
+++ migration/app.sql
@@ -6,19 +6,19 @@ CREATE TABLE users (
 	email VARCHAR(225) NOT NULL,
 	passwords VARCHAR(225) NOT NULL,
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_date TIMESTAMP
+	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
-	post_content,
+	post_content VARCHAR(225),
+	post_image BYTEA,
 	like_count INT NOT NULL,
 	comment_count INT NOT NULL,
 	repost_count INT NOT NULL,
 	repost_id INT,
-	created_date DATE NOT NULL,
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_date TIMESTAMP,
+	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
@@ -26,7 +26,7 @@ CREATE TABLE posts (
 CREATE TABLE post_like (
 	like_id SERIAL PRIMARY KEY,
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_date TIMESTAMP,
+	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
@@ -37,7 +37,7 @@ CREATE TABLE post_comment (
 	comment_id SERIAL PRIMARY KEY,
 	comment_content VARCHAR(225),
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	updated_date TIMESTAMP,
+	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
