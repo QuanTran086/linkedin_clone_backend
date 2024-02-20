537bb925f9a2715735823a3645ba10f1350ce4af
Minh Quan <minhquan862004@gmail.com>
2024-02-20 14:50:53 +0200

    Update SQL query

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/update-password", async (request, response) => {
 });
 
 app.get("/posts", async (request, response) => {
-    const result = await client.query("SELECT users.username, users.description, posts.post_content, posts.like_count, posts.comment_count, posts.repost_count FROM posts JOIN users ON posts.user_id = users.user_id")
+    const result = await client.query("SELECT users.username, users.description, posts.post_content, posts.like_count, posts.comment_count, posts.repost_count FROM posts FULL OUTER JOIN users ON posts.user_id = users.user_id")
     response.json(result.rows)
 })
 
