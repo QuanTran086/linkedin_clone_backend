03aeef83da82a09c0049309053c986d85f04464c
Minh Quan <minhquan862004@gmail.com>
2024-02-26 19:14:00 +0200

    Update database SQL

--- migration/app.sql
+++ migration/app.sql
@@ -13,9 +13,9 @@ CREATE TABLE posts (
     post_id SERIAL PRIMARY KEY,
 	post_content VARCHAR(225),
 	post_image BYTEA,
-	like_count INT NOT NULL,
-	comment_count INT NOT NULL,
-	repost_count INT NOT NULL,
+	like_count INT DEFAULT 0,
+	comment_count INT DEFAULT 0,
+	repost_count INT DEFAULT 0,
 	repost_id INT,
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -43,18 +43,3 @@ CREATE TABLE post_comment (
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
 	FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
-
-SELECT * FROM users
-
-SELECT * FROM posts
-
-SELECT 
-	users.username, 
-	users.description, 
-	posts.post_content, 
-	posts.like_count, 
-	posts.comment_count, 
-	posts.repost_count
-FROM posts 
-JOIN users 
-ON posts.user_id =  users.user_id
