2d4423c945443ad1db491e82b26643782c580723
Minh Quan <minhquan862004@gmail.com>
2024-03-17 16:51:35 +0200

    Update SQL command

--- migration/app.sql
+++ migration/app.sql
@@ -27,7 +27,7 @@ CREATE TABLE post_like (
 	like_id SERIAL PRIMARY KEY,
 	created_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	updated_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	status BOOLEAN NOT NULL DEFAULT false,
+	status BOOLEAN,
 	user_id INT,
 	post_id INT,
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
